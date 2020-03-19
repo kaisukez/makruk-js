d2782954666094c77732626a28f30c690bd13c47
kaisukez <zykigix@hotmail.com>
2020-03-19 20:20:19 +0700

    san regex

--- implementations/0x88/index.js
+++ implementations/0x88/index.js
@@ -275,51 +275,53 @@ const step = R.pipe(
     })
 )
 
-// https://stackoverflow.com/questions/40007937/regex-help-for-chess-moves-san
-// https://stackoverflow.com/questions/12317049/how-to-split-a-long-regular-expression-into-multiple-lines-in-javascript
-const notationRegex = [
-    /(?<piece>[brqnkBRQNK])(?<to>[a-h][1-8])|/,
-    /(?<piece>[brqnkBRQNK])(?<fromFile>[a-h])x(?<to>[a-h][1-8]|)/,
-    /(?<piece>[brqnkBRQNK])(?<from>[a-h][1-8])x(?<to>[a-h][1-8])/,
-    /(?<piece>[brqnkBRQNK])(?<from>[a-h][1-8])(?<to>[a-h][1-8])/,
-    /(?<piece>[brqnkBRQNK])(?<fromFile>[a-h])(?<to>[a-h][1-8])/,
-    /(?<piece>[brqnkBRQNK])x(?<to>[a-h][1-8])/,
-    /(?<fromFile>[a-h])x(?<to>[a-h][1-8])=(?<promotion>(b+r+q+n+B+R+Q+N))/,
-    /(?<fromFile>[a-h])x(?<to>[a-h][1-8])/,
-    /(?<from>[a-h][1-8])x(?<to>[a-h][1-8])=(?<promotion>(b+r+q+n+B+R+Q+N))/,
-    /(?<from>[a-h][1-8])x(?<to>[a-h][1-8])/,
-    /(?<from>[a-h][1-8])(?<to>[a-h][1-8])=(?<promotion>(b+r+q+n+B+R+Q+N))/,
-    /(?<from>[a-h][1-8])(?<to>[a-h][1-8])/,
-    /(?<from>[a-h][1-8])=(?<promotion>(b+r+q+n+B+R+Q+N))/,
-    /(?<to>[a-h][1-8])/,
-    /(?<piece>[brqnkBRQNK])(?<fromRank>[1-8])x(?<to>[a-h][1-8])/,
-    /(?<piece>[brqnkBRQNK])(?<fromRank>[1-8])(?<to>[a-h][1-8])/,
-]
+// // https://stackoverflow.com/questions/40007937/regex-help-for-chess-moves-san
+// // https://stackoverflow.com/questions/12317049/how-to-split-a-long-regular-expression-into-multiple-lines-in-javascript
+// const notationRegex = [
+//     /(?<piece>[brqnkBRQNK])(?<to>[a-h][1-8])|/,
+//     /(?<piece>[brqnkBRQNK])(?<fromFile>[a-h])x(?<to>[a-h][1-8]|)/,
+//     /(?<piece>[brqnkBRQNK])(?<from>[a-h][1-8])x(?<to>[a-h][1-8])/,
+//     /(?<piece>[brqnkBRQNK])(?<from>[a-h][1-8])(?<to>[a-h][1-8])/,
+//     /(?<piece>[brqnkBRQNK])(?<fromFile>[a-h])(?<to>[a-h][1-8])/,
+//     /(?<piece>[brqnkBRQNK])x(?<to>[a-h][1-8])/,
+//     /(?<fromFile>[a-h])x(?<to>[a-h][1-8])=(?<promotion>(b+r+q+n+B+R+Q+N))/,
+//     /(?<fromFile>[a-h])x(?<to>[a-h][1-8])/,
+//     /(?<from>[a-h][1-8])x(?<to>[a-h][1-8])=(?<promotion>(b+r+q+n+B+R+Q+N))/,
+//     /(?<from>[a-h][1-8])x(?<to>[a-h][1-8])/,
+//     /(?<from>[a-h][1-8])(?<to>[a-h][1-8])=(?<promotion>(b+r+q+n+B+R+Q+N))/,
+//     /(?<from>[a-h][1-8])(?<to>[a-h][1-8])/,
+//     /(?<from>[a-h][1-8])=(?<promotion>(b+r+q+n+B+R+Q+N))/,
+//     /(?<to>[a-h][1-8])/,
+//     /(?<piece>[brqnkBRQNK])(?<fromRank>[1-8])x(?<to>[a-h][1-8])/,
+//     /(?<piece>[brqnkBRQNK])(?<fromRank>[1-8])(?<to>[a-h][1-8])/,
+// ]
 
-/**
- * If there's moveObject.notation then use it, if not then use moveObject.from and moveObject.to.
- * 
- * @param {Object} moveObject 
- * @param {String} moveObject.notation algebraic notation like ne2 (knight move) or c5 (bia move)
- * @param {String} moveObject.from algebraic square like d3 or f7
- * @param {String} moveObject.to algebraic square like d3 or f7
- * 
- */
-const extract0x88Move = R.cond([
-    [
-        R.both(R.has('notation'), R.T),
-        R.pipe(
-            R.prop('notation'),
-            R.match(notationRegex),
-            R.tap(console.log),
-            R.applySpec({
-                piece: R.prop(1),
-                from: R.prop(2),
-                to: R.prop(3)
-            })
-        )
-    ]
-])
+const sanRegex = /^(?<piece>[FEMTRK])?(?<fromFlie>[a-h])?(?<fromRank>[1-8])?(?<capture>[x:])?(?<to>[a-h][1-8])(?<promotion>=(?<promoteTo>[F]))?(?<check>(?<normalCheck>[+†])|(?<doubleCheck>\+{2}|‡))?(?<checkmate>[#≠])?$/
+
+// /**
+//  * If there's moveObject.notation then use it, if not then use moveObject.from and moveObject.to.
+//  * 
+//  * @param {Object} moveObject 
+//  * @param {String} moveObject.notation algebraic notation like ne2 (knight move) or c5 (bia move)
+//  * @param {String} moveObject.from algebraic square like d3 or f7
+//  * @param {String} moveObject.to algebraic square like d3 or f7
+//  * 
+//  */
+// const extract0x88Move = R.cond([
+//     [
+//         R.both(R.has('notation'), R.T),
+//         R.pipe(
+//             R.prop('notation'),
+//             R.match(notationRegex),
+//             R.tap(console.log),
+//             R.applySpec({
+//                 piece: R.prop(1),
+//                 from: R.prop(2),
+//                 to: R.prop(3)
+//             })
+//         )
+//     ]
+// ])
 
 // parses all of the decorators out of a SAN string
 const stripped_san = move => {
@@ -533,5 +535,5 @@ console.log(ascii(state.boardState))
 // console.log('step', R.omit(['boardState'])(step(state)))
 
 // console.log(extract0x88Move({ notation: 're2' }))
-// console.log(notationRegex.map(regex => 'naf3'.match(regex)))
-console.log(move_from_san(state.boardState, 'Me2'))
+console.log('e7=F≠'.match(sanRegex))
+// console.log(move_from_san(state.boardState, 'Me2'))
