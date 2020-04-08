f31a80905d57b26cf4fb6921a844e5462dc45196
kaisukez <zykigix@hotmail.com>
2020-04-08 20:10:45 +0700

    prepare to write move testing

---
+++ test/move.test.js
@@ -0,0 +1,89 @@
+const {
+    WHITE,
+    BLACK,
+
+    BIA,
+    FLIPPED_BIA,
+    MA,
+    THON,
+    MET,
+    RUA,
+    KHUN,
+
+    INITIAL_FEN,
+
+    BIA_MOVE_OFFSETS,
+    BIA_ATTACK_OFFSETS,
+    THON_MOVE_OFFSETS,
+    THON_ATTACK_OFFSETS,
+    PIECE_MOVE_OFFSETS,
+    PIECE_ATTACK_OFFSETS,
+
+    IS_SLIDING_PIECE,
+
+    PIECE_POWER,
+
+    SQUARES,
+    FIRST_SQUARE,
+    LAST_SQUARE,
+
+    FLAGS,
+    BITS,
+
+    RANK_1,
+    RANK_2,
+    RANK_3,
+    RANK_4,
+    RANK_5,
+    RANK_6,
+    RANK_7,
+    RANK_8,
+    
+    FILE_A,
+    FILE_B,
+    FILE_C,
+    FILE_D,
+    FILE_E,
+    FILE_F,
+    FILE_G,
+    FILE_H,
+
+    PIECE_POWER_COUNTDOWN,
+    BOARD_POWER_COUNTDOWN
+} = require('../src/constants')
+
+const {
+    swapColor,
+    getAttackOffsets,
+    getMoveOffsets,
+    rank,
+    file,
+    squareColor,
+    algebraic,
+    ascii,
+    clone,
+    compose,
+    pipe
+} = require('../src/utils')
+
+const {
+    extractInfoFromFen,
+    getBoardStateFromBoardString,
+    forEachPieceFromBoardState,
+    getPiecePositions,
+    forEachPiece,
+    countPiece,
+    evalulatePower,
+    updatePiecePositionDictionary,
+    importFen,
+    exportFen
+} = require('../src/state')
+
+
+
+describe('canThisColorAttackThisSquare', () => {
+    // TODO
+    test('white bia', () => {
+        const fen = 'k7/8/8/3B4/8/8/8/7K'
+    })
+})
